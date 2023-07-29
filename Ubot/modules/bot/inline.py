74e9ca687f1e56ab52b932da0afef17216ad7cb2
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-29 14:50:53 +0700

    Update inline.py

--- Ubot/modules/bot/inline.py
+++ Ubot/modules/bot/inline.py
@@ -82,9 +82,9 @@ async def alive_function(message, answers):
         f"<b>Arab-UbotPrem ✅</b>\n"
         f"<b> status: Premium {status} </b>\n"
         f"    <b> expired:</b> <code>{remaining_days}</code>\n"
-        f"    <b> ping_ubot:</b> <code>{ping} ms</code>\n"
-        f"    <b> peer_ubot:</b> <code>{user}</code>\n"
-        f"    <b> uptime_ubot:</b> <code>{uptime}</code>\n")
+        f"    <b> ping_uprem:</b> <code>{ping} ms</code>\n"
+        f"    <b> peer_uprem:</b> <code>{user}</code>\n"
+        f"    <b> uptime_uprem:</b> <code>{uptime}</code>\n")
     answers.append(
         InlineQueryResultArticle(
             title="alive",
