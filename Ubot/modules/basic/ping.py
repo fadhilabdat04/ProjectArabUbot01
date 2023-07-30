1cdd0c1963b12f184a9bf2457e251c1ba2658807
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-30 19:02:33 +0700

    Update ping.py

--- Ubot/modules/basic/ping.py
+++ Ubot/modules/basic/ping.py
@@ -144,7 +144,7 @@ async def pingme(client, message):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await message.reply_text(
-        f"<b>⌬ Arab - UbotPrem!!</b>\n╰━**⌬ Pong!** `%sms`\n╰━ <b> Masa Hidup:</b> {uptime}" % (duration)
+        f"<b>Arab - UbotPrem! 🍀</b>\n❏** Pong!!!** `%sms`\n╰━ <b> Masa Hidup:</b> {uptime}" % (duration)
         )
     await ping_.delete()
 
