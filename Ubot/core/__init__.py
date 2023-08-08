efd5c620b6edbbae5e089d2ddb5e7cc7da8b3a35
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-08 21:14:55 +0700

    Update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -11,9 +11,9 @@ from .constants import *
 
 async def ajg(client):
     try:
-        await client.join_chat("SiArabSupport")
-        await client.join_chat("SiArab_Store")
-        await client.join_chat("cehasiarab")
+        await client.join_chat("GroupSiArab")
+        await client.join_chat("JasaSiArab")
+        await client.join_chat("ArabCodee")
         await client.join_chat("lpm_bbg_ddk14")
         await client.join_chat("SXID_IN_GAME")
     except BaseException:
